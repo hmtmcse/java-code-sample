6a52efc8fa7eea9f9e60028dd3a2250e5ac75b03
Touhid Mia <hmtm.cse@gmail.com>
2020-08-03 10:01:32 +0600

    Reflection

--- src/test/java/com/hmtmcse/mockito/MockitoTest.java
+++ src/test/java/com/hmtmcse/mockito/MockitoTest.java
@@ -2,7 +2,6 @@ package com.hmtmcse.mockito;
 
 import org.junit.Test;
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 public class MockitoTest {
 
@@ -10,6 +9,5 @@ public class MockitoTest {
     public void test1()  {
         MockitoClass test = mock(MockitoClass.class);
         when(test.isAvailable()).thenReturn(true);
-        assertEquals(test.isAvailable(), true);
     }
 }
